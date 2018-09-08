6d74c008afa350645832d5b196a33e1b2351a04f
Hussein Hazimeh <hh@ieee.org>
2018-09-08 02:04:52 -0400

    Update CDL012Swaps.cpp

--- src/CDL012Swaps.cpp
+++ src/CDL012Swaps.cpp
@@ -15,7 +15,7 @@ FitResult CDL012Swaps::Fit()
     for (unsigned int t = 0; t < MaxNumSwaps; ++t)
     {
         std::cout<<"1Swaps Iteration: "<<t<<". "<<"Obj: "<<objective<<std::endl;
-        //B.print();
+        B.print();
         arma::sp_mat::const_iterator start = B.begin();
         arma::sp_mat::const_iterator end   = B.end();
         std::vector<unsigned int> NnzIndices;
@@ -37,7 +37,7 @@ FitResult CDL012Swaps::Fit()
 
         for (auto& i : NnzIndices)
         {
-
+            std::cout<<"i: "<<i<<std::endl;
 
             arma::rowvec riX = (r + B[i] * X->unsafe_col(i)).t() * *X; // expensive computation ## call the new function here, inlined? ##
             //std::cout<<"HERE1!!!" << std::endl;
@@ -58,13 +58,13 @@ FitResult CDL012Swaps::Fit()
 
             //if(maxcorr > (1 + 2 * ModelParams[2])*std::fabs(B[i]) + ModelParams[1] )
             arma::vec ri = r + B[i] * X->unsafe_col(i);
-            std::cout<<"HERE1!!!" << std::endl;
             double betaj = (riX[maxindex] - std::copysign(ModelParams[1], riX[maxindex])) / (1 + 2 * ModelParams[2]);
-            std::cout<<"HERE2!!!" << std::endl;
-            if (0.5*arma::dot(r,r) + ModelParams[0] + ModelParams[1]*std::fabs(B[i]) + ModelParams[2]*B[i]*B[i] > 0.5*arma::dot(ri,ri) - riX[maxindex]*betaj + 0.5*betaj*betaj + ModelParams[0] + ModelParams[1]*std::fabs(betaj) + ModelParams[2]*betaj*betaj)
+            double initialobj = 0.5*arma::dot(r,r) + ModelParams[0] + ModelParams[1]*std::fabs(B[i]) + ModelParams[2]*B[i]*B[i];
+            double swapobj = 0.5*arma::dot(ri,ri) - riX[maxindex]*betaj + 0.5*betaj*betaj + ModelParams[0] + ModelParams[1]*std::fabs(betaj) + ModelParams[2]*betaj*betaj
+            if ( initialobj > swapobj)
             {
-                std::cout<<"HERE3!!!" << std::endl;
-                std::cout<<"!!!!! Swapping !!!! " <<std::endl;
+                std::cout<<" initial obj = " << initialobj <<" swap obj = " <<swapobj <<std::endl;
+                std::cout<<"!!!!! Swapping "<<i <<" with " << maxindex <<std::endl;
                 std::cout<<"Obj before "<<objective<<std::endl;
                 B[i] = 0;
                 B[maxindex] = (riX[maxindex] - std::copysign(ModelParams[1], riX[maxindex])) / (1 + 2 * ModelParams[2]);
